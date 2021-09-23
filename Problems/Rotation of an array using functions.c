a672fcf3170b5f31f4fb00b5fb461a4075777068
Anjura <85027663+Anjura@users.noreply.github.com>
2021-09-23 23:27:52 +0530

    Create Rotation of an array using functions.c

---
+++ Problems/Rotation of an array using functions.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void main()
+{
+    int arr[] = { 1, 2, 3, 4, 5 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+ 
+    reverse(arr, n, 0, 0);
+    display(arr, n);
+    
+    reverse(arr, n, 1, 4);
+    display(arr, n);
+    
+    reverse(arr, n, 0, 4);
+    display(arr, n);
+ 
+}
+
+void reverse(int arr[], int n, int start, int end)
+{
+    for ( ; start < end; start++, end--)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
+
+void display(int arr[], int n)
+{
+	int i;
+    for ( i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+ 
